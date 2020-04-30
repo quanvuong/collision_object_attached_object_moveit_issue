d859238b964a852f97ec27218836c37803dd8d38
ubuntu desktop <quan.hovuong@gmail.com>
2020-04-30 11:38:24 -0700

    rm devel and build from git history

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/quan/moveit_ws/src
-# Build directory: /home/quan/moveit_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(attach_obj)
--- build/attach_obj/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/quan/moveit_ws/src/attach_obj
-# Build directory: /home/quan/moveit_ws/build/attach_obj
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
